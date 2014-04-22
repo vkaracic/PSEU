852e793f4a86e8fd6624e7adfc0e176ec6d3651f
vkaracic <karacicvedran@gmail.com>
2014-04-22 17:31:39 +0200

    dodane nove funkcije #2

--- project_x/scnd_try.py
+++ project_x/scnd_try.py
@@ -4,7 +4,7 @@ import networkx as nx
 
 # importiranje datoteke citavog grafa koja je obliku Cmap propozicija:
 # koncept \t veza \t koncept\n
-def import_graf():
+def ucitaj_graf():
     G = nx.DiGraph() # Stvaranje strukture grafa
     f = open('test_mapa.txt', 'r')
     for line in f:
@@ -41,17 +41,49 @@ def spremi_graf(G, naziv):
     f.close
 
 
-def main():
-    G = import_graf()
-    korjen0 = nadji_korjen(G)[0] # u testnoj mapi se nalaze dva korjena pa zbog toga se na kraj dodaje [1] da se uzme korjen 'START' jer ima više elemenata
-    korjen1 = nadji_korjen(G)[1]
+# Trazi podstablo zadane dubine od zadanog korjena 
+def nadji_jedno_podstablo(G, korjen, dubina):
+    if dubina == 0: return # ako je dubina premasena prekida rekurziju
+        
+    susjedi = G.neighbors(korjen) # trazi susjede od zadanog korjena
+    #print susjedi
 
-    STARTgraph = nadji_cPodstablo(G, korjen1)
-    nazivDat = raw_input('Naziv datoteke: ')
-    spremi_graf(STARTgraph, nazivDat)
+    tmpGraf.add_nodes_from(susjedi) # dodaj susjede u globalni graf
+
+    for item in susjedi:
+        tmpGraf.add_edge(korjen, item) # napravi vezu izmedju trenutnog susjeda i njegovog korjena
+        nadji_jedno_podstablo(G, item, dubina-1) # funkcija se ponavlja s tim da korjeni budu elementi iz prethodnih susjeda a dubina se smanjuje
+    return tmpGraf
+
+
+def nadji_sva_podstabla(G, korjen, dubina):
+    
+    pocetni_graf = nadji_jedno_podstablo(G, korjen, dubina) # trazi pocetni graf
+    spremi_graf(pocetni_graf, korjen) # sprema pocetni graf u zasebnu datoteku
+
+    listovi = [n for n,d in pocetni_graf.out_degree().items() if d==0] # trazenje listova pocetnog grafa
+    for el in listovi:
+        graf = nadji_jedno_podstablo(G, el, dubina)
+        spremi_graf(graf, el) # spremanje grafa u svoju datoteku
+        tmpGraf.clear() # brisanje elemenata globalnog grafa zbog sljedece iteracije
 
     
 
+
+
+
+def main():
+    global tmpGraf
+    tmpGraf = nx.DiGraph()
+
+    G = ucitaj_graf()
+    korjen0 = nadji_korjen(G)[0] # u testnoj mapi se nalaze dva korjena pa zbog toga se na kraj dodaje [1] da se uzme korjen 'START' jer ima više elemenata
+    korjen1 = nadji_korjen(G)[1]
+
+    subG1 = nadji_cPodstablo(G, korjen1)
+    tmpGraf.add_node(korjen1)
+
+    nadji_sva_podstabla(subG1, korjen1, 3)
     
     
 
