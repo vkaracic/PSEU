fbe34039dc89a5067d8cc162e4cc40d746e1c361
vkaracic <karacicvedran@gmail.com>
2014-04-22 16:17:17 +0200

    dodane nove funkcije

--- project_x/scnd_try.py
+++ project_x/scnd_try.py
@@ -22,17 +22,38 @@ def nadji_korjen(G):
 
 # Pronalazi cijelo podstablo danog korjena
 def nadji_cPodstablo(G, korjen):
-    udG = G.to_undirected()
-    nodes = nx.shortest_path(udG, korjen).keys()
-    return nodes
+    udG = G.to_undirected() # pretvaranje u undirected graf da bi se mogla koristiti funkcija shortest_path
+                            # http://stackoverflow.com/questions/13914920/networkx-extract-the-smallest-connected-subgraph
+    nodes = nx.shortest_path(udG, korjen).keys() # svi elemetni povezani s korjenom
+    podstablo = G.subgraph(nodes) # stvaranje podgrafa od dobijenih elemenata
+    DiStablo = podstablo.to_directed() # vracanje tog podstabla u strukturu usmjerenog grafa
+    return DiStablo
+
+
+# Funkcija za spremanje grafa u tekstualnu datoteku
+def spremi_graf(G, naziv):
+    datoteka = '%s.txt' % naziv
+    f = open(datoteka, 'w') 
+    for el in G.edges():
+        it1, it2 = el
+        string = "%s\tima\t%s\n" % (it1, it2)
+        f.write(string)
+    f.close
 
 
 def main():
     G = import_graf()
-    prviKorjen = nadji_korjen(G)[1]
-    #print nadji_cPodstablo(G, prviKorjen)
-    nodes = [d for d in G.nodes_iter()]
-    print nodes
+    korjen0 = nadji_korjen(G)[0] # u testnoj mapi se nalaze dva korjena pa zbog toga se na kraj dodaje [1] da se uzme korjen 'START' jer ima više elemenata
+    korjen1 = nadji_korjen(G)[1]
+
+    STARTgraph = nadji_cPodstablo(G, korjen1)
+    nazivDat = raw_input('Naziv datoteke: ')
+    spremi_graf(STARTgraph, nazivDat)
+
+    
+
+    
+    
 
 if __name__ == '__main__':
     main()
